06983279ec9f9ff834747e27147a89cbec359f07
Cyeciad <loginmish@gmail.com>
2025-03-18 18:13:57 +0500

    sixth task added

--- src/index.js
+++ src/index.js
@@ -72,9 +72,54 @@ class Trasher extends Dog {
             return [...baseDescriptions, abilityDescription];
         };
     }
-
-
 }
+
+class Lad extends Dog {
+    constructor() {
+        super('Браток', 2);
+    }
+
+    static getInGameCount() {
+        return this.inGameCount || 0;
+    }
+
+    static setInGameCount(value) {
+        this.inGameCount = value;
+    }
+
+    static getBonus() {
+        const count = this.getInGameCount();
+        return count * (count + 1) / 2;
+    }
+
+    doAfterComingIntoPlay(gameContext, continuation) {
+        Lad.setInGameCount(Lad.getInGameCount() + 1);
+        continuation();
+    }
+
+    doBeforeRemoving(continuation) {
+        Lad.setInGameCount(Lad.getInGameCount() - 1);
+        continuation();
+    }
+
+    modifyDealedDamageToCreature(value, toCard, gameContext, continuation) {
+        continuation(value + Lad.getBonus());
+    }
+
+
+    getDescriptions() {
+        const baseDescriptions = Card.prototype.getDescriptions.call(this);
+
+        if (Lad.prototype.hasOwnProperty('modifyDealedDamageToCreature') ||
+            Lad.prototype.hasOwnProperty('modifyTakenDamage')) {
+            baseDescriptions.push("Чем их больше, тем они сильнее");
+        }
+
+        return baseDescriptions;
+    }
+}
+
+
 // Отвечает является ли карта уткой.
 function isDuck(card) {
     return card && card.quacks && card.swims;
@@ -105,12 +150,8 @@ const seriffStartDeck = [
     new Duck(),
     new Duck(),
     new Duck(),
-    new Gatling(),
 ];
 const banditStartDeck = [
-    new Trasher(),
-    new Dog(),
-    new Dog(),
     new Trasher(),
 ];
 
